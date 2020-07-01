4c3f0c9b10c9cec417373036ebb9d2a0af5c25be
JudeCornish <judecornish@gmail.com>
2020-07-01 02:02:03 +0100

    tables

--- app.js
+++ app.js
@@ -89,3 +89,17 @@ function isElementInViewport(el) {
 				(window.innerHeight || document.documentElement.clientHeight))
 	);
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+	myFunction();
+};
+
+function myFunction() {
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height =
+		document.documentElement.scrollHeight -
+		document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById("myBar").style.width = scrolled + "%";
+}
